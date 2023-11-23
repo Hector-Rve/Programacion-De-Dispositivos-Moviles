172b254b726ecce36cc67ea5a8c8adb34fdcd748
Hector-Rve <143782044+Hector-Rve@users.noreply.github.com>
2023-11-23 03:56:10 -0600

    TRABAJO EN CLASE

---
+++ TRABAJO EN CLASE/2do Corte/Vehículo/Clase Vehículo Constructor 2.kt
@@ -0,0 +1,9 @@
+//Parte 2 complementaria de la practica Inicial Vehiculo Constructor
+class P9_Vehiculo_2 {
+    init {
+        println("Vehiculo creado")
+    }
+    companion object Factory{
+        fun create(): P9_Vehiculo_2 = P9_Vehiculo_2()
+    }
+}
